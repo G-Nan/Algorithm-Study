55c2726c65af72124f5ffa674c4c97461b03001f
cherry-kiwi <86244674+cherry-kiwi@users.noreply.github.com>
2023-02-22 18:34:19 +0900

    Update 155652_둘만의 암호.py

---
+++ cherry-kiwi/1주차/155652_둘만의 암호.py
@@ -0,0 +1,8 @@
+def solution(s, skip, index):
+    answer = ''
+    for i in range(s):
+        answer=[s[i]+index]
+        
+        if skip in s: #skip이 있으면
+            #skip 만큼 제외하고 건너뛰기
+    return answer
