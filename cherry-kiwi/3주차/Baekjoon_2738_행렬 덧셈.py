a5a7fdc01a64dedeebdf43203a09f98fe5e71bf1
Gnan <97678547+G-Nan@users.noreply.github.com>
2023-03-13 18:40:25 +0900

    update note

--- cherry-kiwi/3주차/Baekjoon_2738_행렬 덧셈.py
+++ cherry-kiwi/3주차/Baekjoon_2738_행렬 덧셈.py
@@ -4,5 +4,5 @@ b = [list(map(int, input().split())) for _ in range(n)]
 
 for row in range(n):
     for col in range(m):
-        print(a[row][col]+b[row][col], end="")
+        print(a[row][col]+b[row][col], end=" ")
     print()
